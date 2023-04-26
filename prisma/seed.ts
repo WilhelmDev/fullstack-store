e1b87be93c6231bc3006bd675c97ca1ac199299a
WilhelmDev <wiljoseflores@gmail.com>
2023-04-26 14:39:19 -0400

    first-steps

---
+++ prisma/seed.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client'
+import {categorias} from './data/categorys'
+import { productos } from './data/products'
+
+const prisma = new PrismaClient()
+
+const main = async ():Promise<void> => {
+    try {
+        await prisma.category.createMany({
+            data: categorias
+        })
+        await prisma.product.createMany({
+            data: productos
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+main()
